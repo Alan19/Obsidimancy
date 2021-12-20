f703f7f77c031b0e9ebc001d5e7ebb115b3ebed7
Alan19 <Alan19@users.noreply.github.com>
2021-12-20 01:19:31 -0500

    Switch to constant in Skydiver's Hood

--- src/main/java/com/pursuitofglowstone/obsidimancy/items/SkydiversHood.java
+++ src/main/java/com/pursuitofglowstone/obsidimancy/items/SkydiversHood.java
@@ -9,12 +9,15 @@ import net.minecraftforge.fml.common.Mod;
 
 @Mod.EventBusSubscriber
 public class SkydiversHood extends ArmorItem {
+
+    public static final float DAMAGE_MULTIPLIER = .85f;
+
     public SkydiversHood() {
         super(ArmorMaterials.LEATHER, EquipmentSlot.HEAD, new Item.Properties().tab(ObsidimancyItems.TAB_OBSIDIMANCY).rarity(Rarity.UNCOMMON));
     }
 
     // TODO Make this affected by configs
     public float getDamageMultiplier() {
-        return .85f;
+        return DAMAGE_MULTIPLIER;
     }
 }
