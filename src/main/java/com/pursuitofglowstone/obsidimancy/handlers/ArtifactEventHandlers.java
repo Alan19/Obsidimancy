9d6b151f87de7162351e615664cf6b0ca0dd8b5e
Alan19 <Alan19@users.noreply.github.com>
2021-12-20 01:15:06 -0500

    Restore precursor pickaxe functionality

--- src/main/java/com/pursuitofglowstone/obsidimancy/handlers/ArtifactEventHandlers.java
+++ src/main/java/com/pursuitofglowstone/obsidimancy/handlers/ArtifactEventHandlers.java
@@ -1,9 +1,12 @@
 package com.pursuitofglowstone.obsidimancy.handlers;
 
+import com.pursuitofglowstone.obsidimancy.items.ObsidimancyItems;
 import com.pursuitofglowstone.obsidimancy.items.SkydiversHood;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.event.entity.living.LivingFallEvent;
+import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
@@ -13,9 +16,16 @@ public class ArtifactEventHandlers {
     public static void onFallDamage(LivingFallEvent event) {
         final LivingEntity entityLiving = event.getEntityLiving();
         for (ItemStack itemStack : entityLiving.getArmorSlots()) {
-            if (itemStack.getItem() instanceof SkydiversHood) {
-                event.setDamageMultiplier(event.getDamageMultiplier() * ((SkydiversHood) itemStack.getItem()).getDamageMultiplier());
+            if (itemStack.getItem() instanceof SkydiversHood hood) {
+                event.setDamageMultiplier(event.getDamageMultiplier() * hood.getDamageMultiplier());
             }
+        }
+    }
+
+    @SubscribeEvent
+    public static void harvestObsidian(PlayerEvent.HarvestCheck event) {
+        if (event.getPlayer().getMainHandItem().getItem() == ObsidimancyItems.PRECURSOR_PICKAXE.get() || event.getTargetBlock().getBlock() == Blocks.OBSIDIAN) {
+            event.setCanHarvest(true);
         }
     }
 }
--- src/main/java/com/pursuitofglowstone/obsidimancy/items/PrecursorPickaxe.java
+++ src/main/java/com/pursuitofglowstone/obsidimancy/items/PrecursorPickaxe.java
@@ -1,13 +1,20 @@
 package com.pursuitofglowstone.obsidimancy.items;
 
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.PickaxeItem;
 import net.minecraft.world.item.Tiers;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockState;
+import org.jetbrains.annotations.NotNull;
 
 public class PrecursorPickaxe extends PickaxeItem {
     public PrecursorPickaxe() {
         super(Tiers.IRON, 1, -2.8F, (new Item.Properties()).tab(ObsidimancyItems.TAB_OBSIDIMANCY));
     }
 
-    // TODO Reimplement tier increasing effect
+    @Override
+    public float getDestroySpeed(@NotNull ItemStack itemStack, BlockState blockState) {
+        return blockState.getBlock() == Blocks.OBSIDIAN ? speed : super.getDestroySpeed(itemStack, blockState);
+    }
 }
