5b22bb1957595d23db79dd79bb7a647b8d56cc52
alanx <alanxiao44@gmail.com>
2023-07-16 18:33:06 -0400

    Add more tags for Obsidimancy

--- src/main/java/com/pursuitofglowstone/obsidimancy/data/ObsidimancyItemTags.java
+++ src/main/java/com/pursuitofglowstone/obsidimancy/data/ObsidimancyItemTags.java
@@ -5,6 +5,7 @@ import com.pursuitofglowstone.obsidimancy.tags.ObsidimancyTags;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.tags.BlockTagsProvider;
 import net.minecraft.data.tags.ItemTagsProvider;
+import net.minecraftforge.common.Tags.Items;
 import net.minecraftforge.common.data.ExistingFileHelper;
 
 import javax.annotation.Nullable;
@@ -17,9 +18,12 @@ public class ObsidimancyItemTags extends ItemTagsProvider {
         super(generator, blockTagsProvider, Obsidimancy.MOD_ID, existingFileHelper);
     }
 
+    @SuppressWarnings("unchecked")
     @Override
     public void addTags() {
         tag(ObsidimancyTags.PRECURSOR_PICKAXES).add(PRECURSOR_PICKAXE.get(), NETHER_PRECURSOR_PICKAXE.get(), ENDER_PRECURSOR_PICKAXE.get());
         tag(ObsidimancyTags.OBSIDIAN_SHARDS).add(OBSIDIAN_SHARD.get(), OVERWORLD_SHARD.get(), NETHER_SHARD.get(), ENDER_SHARD.get());
+        tag(Items.TOOLS_PICKAXES).addTags(ObsidimancyTags.PRECURSOR_PICKAXES);
+        tag(Items.ARMORS_HELMETS).add(SKYDIVERS_HOOD.get(), OVERWORLD_SKYDIVERS_HOOD.get(), NETHER_SKYDIVERS_HOOD.get(), ENDER_SKYDIVERS_HOOD.get());
     }
 }
